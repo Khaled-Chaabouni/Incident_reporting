139afde0dd1199836f5e2f515c646ca16ae17a5b
Khaled Chaabouni <upmnkr.ypmmfsczi@gmail.com>
2023-08-07 10:37:16 +0100

    Database update

--- model/adactim.sql
+++ model/adactim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 05, 2023 at 10:36 PM
+-- Generation Time: Aug 07, 2023 at 09:36 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -30,26 +30,26 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `reports`;
 CREATE TABLE IF NOT EXISTS `reports` (
   `num_tiquet` int(11) NOT NULL AUTO_INCREMENT,
-  `sender` int(11) NOT NULL,
+  `sender` varchar(500) NOT NULL,
   `date` date NOT NULL,
-  `type` text NOT NULL,
+  `type` varchar(500) NOT NULL,
   `content` text NOT NULL,
   `impact` enum('light','moderate','severe') NOT NULL,
   `measures` text NOT NULL,
   PRIMARY KEY (`num_tiquet`),
   KEY `sender` (`sender`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reports`
 --
 
 INSERT INTO `reports` (`num_tiquet`, `sender`, `date`, `type`, `content`, `impact`, `measures`) VALUES
-(2, 44, '2023-08-04', 'sqli', 'on march 23rd we got an sql injection through our login form', 'severe', 'encrypt data'),
-(3, 404, '2023-08-04', 'cookies injection', 'we witnessed admin cookies manipulated by a 3rd party software, used to access database.', 'severe', 'cookies encryption'),
-(25, 404, '2023-08-05', 'sqli', 'qzgzeqg', 'light', 'azqretyy'),
-(24, 404, '2023-08-05', '', '', 'light', ''),
-(23, 404, '2023-08-05', 'phishing attack', 'one of our agents got a fishing email.', 'moderate', 'take down server & add firewall.');
+(2, '251e03d9edf2a255a71b4178ebfaa3ef', '2023-08-04', 'sqli', 'on march 23rd we got an sql injection through our login form', 'severe', 'encrypt data'),
+(3, '251e03d9edf2a255a71b4178ebfaa3ef', '2023-08-04', 'cookies injection', 'we witnessed admin cookies manipulated by a 3rd party software, used to access database.', 'severe', 'cookies encryption'),
+(23, '251e03d9edf2a255a71b4178ebfaa3ef', '2023-08-05', 'phishing attack', 'one of our agents got a fishing email.', 'moderate', 'take down server & add firewall.'),
+(27, 'c2f1e0670917aa6bcd542c6dc53efe91', '2023-08-06', 'social engineering', 'one of our agents gave away his login credentials thinking it was the helpdesk calling. ', 'severe', 'invalidate his credentials'),
+(30, 'de21d9c8de94753f55ad26eb5609738f', '2023-08-07', 'mama', 'mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! mama jeeeeeet jsp koi faaaiiire!! ', 'moderate', 'aqzsegdhjfkglaqzsegdhjfkglaqzsegdhjfkglaqzsegdhjfkglaqzsegdhjfkglaqzsegdhjfkglaqzsegdhjfkgl');
 
 -- --------------------------------------------------------
 
@@ -65,17 +65,28 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(500) NOT NULL,
   `password` varchar(500) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `account_type`, `username`, `email`, `password`) VALUES
-('1', 'simple_user', 'Agent_404', 'Agent404@gmail.com', 'Agent404'),
-('acd9d738bf6177e3de4c0f67fb3c08cb', 'simple_user', 'fea8cfbe46dafe8127c135c27342f4d0', '27a3e9ae2d1cbc1bc3a9e2e15d933e95', 'cfd8a887c09d150dc927a6b9dea684af'),
-('80ca8e237e6889aafb0d869bf80f0fef', 'simple_user', '90fb261a4033f4c52fc7081d49b2c910', '9d95a985c775710477a8cbfec4ab1e3f', '0fd22d00dcbf01858af1cdcb0aebf3ac'),
-('c80179f41f304c5612afc9345b7a297d', 'simple_user', '05620efab28d607dd3a93994e7ba633b', '5ddd7e9b94cbaf8cb44a350c88b0ea34', '65f8fa30fda8e4db735b6179ca14e8ea');
+('251e03d9edf2a255a71b4178ebfaa3ef', 'admin', 'Khaled Chaabouni', 'khaledchaabouni@adactim.com', 'd91ca33809a5c1ff46537361d61b7541'),
+('3040d169e739cbfac273c28f6ee00972', 'simple_user', 'Khaled Chaabouni', 'Chaabounikhaled@gmail.com', 'd91ca33809a5c1ff46537361d61b7541'),
+('45c62d705065b85451578de73ebb4051', 'simple_user', 'test', 'test@ts.com', 'e10adc3949ba59abbe56e057f20f883e'),
+('c2f1e0670917aa6bcd542c6dc53efe91', 'simple_user', 'Rahma Ben Hassen', 'Rahmabenhassen@gmail.com', '2ddbf96e67b1c7d400979c9670d63421'),
+('de21d9c8de94753f55ad26eb5609738f', 'simple_user', 'ahmad mohsen', 'ahmadmohsen@gmail.com', '962762aa8e6f3e7a817e7fbb621c0d51');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_fk` FOREIGN KEY (`sender`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
