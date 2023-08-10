584d141944a8b15785ee73bdbb708bc628e8e4c5
Khaled-Chaabouni <khaled.chaabouni19@gmail.com>
2023-08-10 10:27:18 +0100

    Deleted test file

--- project/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once'model/dbcon.php';
-session_start();
-echo $_SESSION['userid'],"//",$_SESSION['date'],"//",$_SESSION['type'],"//",$_SESSION['content'],"//",$_SESSION['impact'],"//",$_SESSION['measures'];
-
-?>
